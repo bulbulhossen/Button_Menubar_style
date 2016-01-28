d400412473195aaf508dfcbebe274edba4447b3c
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-01-28 16:53:25 +0600

    Initial commit

---
+++ app/src/main/java/com/example/appsd/buttonmenubarstyle/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.appsd.buttonmenubarstyle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        /*details : https://github.com/bulbulhossen/android-segmented-control*/
+    }
+}
